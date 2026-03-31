2c4c4e06522d7d54855ac60a7af4fc44268d5731
Mozes77 <romanminin1977@gmail.com>
2026-03-31 18:28:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -455,4 +455,4 @@ def handle_text(message):
 # =========================
 
 if __name__ == "__main__":
-    bot.infinity_polling(timeout=60, long_polling_timeout=60)
+   bot.infinity_polling(skip_pending=True)
